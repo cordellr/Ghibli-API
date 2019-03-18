1bc039dc29a3969d6f295cf7cdf2d31efc7232b5
cordellr <cordell.rainock@gmail.com>
2019-03-18 16:26:48 -0500

    Pulled in data from Ghibli API

--- src/components/movieContainer.jsx
+++ src/components/movieContainer.jsx
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import MovieBox from "./movieBox";
 
 class MovieContainer extends Component {
-  state = {};
+  //state = {};
+
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      //takes result and sparses into json text
+      .then(res => res.json())
+      .then(obj => console.log(obj));
+  }
+
   render() {
     return <MovieBox />;
   }
