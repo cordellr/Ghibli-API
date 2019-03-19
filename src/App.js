57293398f48a83eba4e83838684ad0fd405735b6
cordellr <cordell.rainock@gmail.com>
2019-03-18 21:41:47 -0500

    Added studio ghibli logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import MovieContainer from "./components/movieContainer";
+import totoro from "./totoro.png";
 
 class App extends Component {
   render() {
-    return <MovieContainer />;
+    return (
+      <div className="d-flex flex-column align-items-center">
+        <img src={totoro} alt="logo" className="img-fluid" />
+        <MovieContainer />
+      </div>
+    );
   }
 }
 
