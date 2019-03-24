aeccdcbd5841d1390d789f716ecb4182b076fa23
cordellr <cordell.rainock@gmail.com>
2019-03-23 21:00:58 -0500

    Matching id to array object from API

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MovieCard = ({ match }) => {
-  console.log(match.params);
+class MovieCard extends Component {
+  state = {
+    filmsArray: []
+  };
 
-  return <h1>{match.params.id}</h1>;
-};
+  //pulls film data from Ghibli API, and sets data to filmsArray in state
+  async componentDidMount() {
+    const apiUrl = "https://ghibliapi.herokuapp.com/films";
+    const filmsArray = await fetch(apiUrl)
+      //takes result and parses into an array of objects
+      .then(res => res.json());
+    //console.log(filmsArray);
+    this.setState({ filmsArray });
+  }
+
+  render() {
+    const filmObj = this.state.filmsArray
+      .map(function(x) {
+        return x.id;
+      })
+      .indexOf(this.props.match.params.id);
+
+    console.log(filmObj);
+    console.log(this.props.match.params);
+    return <h1>{this.props.match.params.id}</h1>;
+  }
+}
 
 export default MovieCard;
