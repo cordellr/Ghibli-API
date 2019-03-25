c7100efb2a171c3688dac06ffde1504b1b0c2288
cordellr <cordell.rainock@gmail.com>
2019-03-24 22:13:04 -0500

    Adjustments to navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <Link className="navbar-brand" to="/">
         Home
       </Link>
